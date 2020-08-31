3b879f8ae1f8e29c6857eaa8218c244648bbcee2
Caleb Bron <caleb@rancher.com>
2020-08-31 12:40:25 -0700

    add prom file

--- server/app.py
+++ server/app.py
@@ -1,15 +1,24 @@
 #!/usr/bin/env python
 
-from flask import Flask, jsonify
-from flask_caching import Cache
 import json
 import time
+
+from flask import Flask, jsonify, Response
+from flask_caching import Cache
+from prometheus_client import Gauge, generate_latest
 import co2meter as co2
 
+
 app = Flask(__name__)
+
 cache = Cache(config={'CACHE_TYPE': 'simple'})
 cache.init_app(app)
 
+# prom data
+curr_co2 = Gauge('co2', 'Current CO2 level')
+curr_temp = Gauge('temp', 'Current temperature in fahrenheit')
+
+# get data from usb device
 def grab_data():
 	mon = co2.CO2monitor()
 	data = mon.read_data() # 0 is time, 1 is co2, 2 is temp
@@ -26,7 +35,10 @@ def json():
 @app.route('/metrics')
 @cache.cached(timeout=30)
 def metrics():
-	return 
+	data = grab_data()
+	curr_co2.set(data["co2"])
+	curr_temp.set(data["temp"])
+	return Response(generate_latest(), mimetype='text/plain; version=0.0.4; charset=utf-8')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+	app.run(debug=False, host='0.0.0.0')
